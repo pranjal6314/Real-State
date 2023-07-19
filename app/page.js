c3576005c7f90db9060c460812b9394f3525e421
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2023-07-19 21:15:58 +0530

    feat : property section added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-// import Property from "../components/Property";
+import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 export const Banner = ({
@@ -53,11 +53,11 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName="/search?purpose=for-rent"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
     />
-    {/* <Flex flexWrap="wrap">
+    <Flex flexWrap="wrap">
       {propertiesForRent.map((property) => (
         <Property property={property} key={property.id} />
       ))}
-    </Flex> */}
+    </Flex>
     <Banner
       purpose="BUY A HOME"
       title1=" Find, Buy & Own Your"
@@ -68,11 +68,11 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
     />
-    {/* <Flex flexWrap="wrap">
+    <Flex flexWrap="wrap">
       {propertiesForSale.map((property) => (
         <Property property={property} key={property.id} />
       ))}
-    </Flex> */}
+    </Flex>
   </Box>
 );
 
